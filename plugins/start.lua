f128ef9d392c93aa5d22826fd58eadfdd93c1876
adminmagma <2bgfuyxtk@30mail.ir>
2016-01-27 18:40:13 +0330

    Create start.lua

---
+++ plugins/start.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "تست تله ماگما"
+end
+
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/start$",
+    "^!Start$",
+    "^/Start$",
+    "^!start$",
+   "^!help$",
+  "^/help$",
+  "^!Help$",
+  },
+  run = run
+}
+
+end
